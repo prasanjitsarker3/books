a7c7538082039570e228a4d04758cfec1bde78aa
prasanjitsarker3 <sarkerprasanjit379@gmail.com>
2023-04-15 17:29:45 +0530

    Code Ready

--- src/components/BookCart/BookCart.jsx
+++ src/components/BookCart/BookCart.jsx
@@ -6,7 +6,7 @@ const BookCart = ({ book }) => {
     return (
         <Link to={`../book/${isbn13}`}>
             <div className='overflow-hidden relative transition duration-200 transform hover:-translate-y-2 rounded shadow-lg hover:shadow-2xl  '>
-                <img src={image} alt="" srcset="" className='object-cover w-full h-56 md:h-64 lg:h-80' />
+                <img src={image} alt="" srcset="" className='object-cover w-full h-96 md:h-64 lg:h-80' />
                 <div className='bg bg-black bg-opacity-75 px-6 py-4 opacity-0 hover:opacity-100 text-gray-300 absolute inset-0 flex flex-col transition-opacity text-center'>
 
                     <p>{title}</p>
